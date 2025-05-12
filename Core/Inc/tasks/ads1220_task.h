7947db4368acfe29d04213053cb24250a5285a6b
gxw1234 <396938551@qq.com>
2025-05-12 22:24:23 +0800

    增加asd1220_1

---
+++ Core/Inc/tasks/ads1220_task.h
@@ -0,0 +1,10 @@
+#ifndef ADS1220_TASK_H
+#define ADS1220_TASK_H
+
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void ADS1220_Task(void *argument);
+
+#endif /* ADS1220_TASK_H */
---
+++ Core/Src/tasks/ads1220_task.c
@@ -0,0 +1,15 @@
+#include "tasks/ads1220_task.h"
+#include <stdio.h>
+
+void ADS1220_Task(void *argument)
+{
+  
+    
+
+
+    while(1) {
+
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
